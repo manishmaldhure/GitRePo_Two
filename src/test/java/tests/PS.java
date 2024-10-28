415f0eb1c40fe90f1cd0b26ac9802dd2b8958fc9
manishmaldhure <manish.maldhure2021@gmail.com>
2024-10-28 18:54:14 +0530

    1st new commit on develop branch

--- src/test/java/tests/PS.java
+++ src/test/java/tests/PS.java
@@ -8,11 +8,14 @@ public class PS {
 		System.out.println("do this method..");
 		System.out.println("Jira 1");
 		System.out.println("Jira 2");
-		System.out.println("Jira 3 addedd on develop branch..");
-		System.out.println("Jira 4 addedd on develop branch..");
+		System.out.println("Jira 3 addedd on  branch..");
+		System.out.println("Jira 4 addedd on  branch..");
 		
 		//#
-		System.out.println("Jira 5 addedd on develop branch..");
+		System.out.println("Jira 5 addedd on  branch..");
+		
+		System.out.println("line 7  addedd on develop   branch..");
+		System.out.println("line 8  addedd on develop   branch..");
 		
 	}
 	
