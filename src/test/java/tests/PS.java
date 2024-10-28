c604e1c578a66071b0880bb04c8d39d56a5acdea
manishmaldhure <manish.maldhure2021@gmail.com>
2024-10-28 16:29:27 +0530

    1st commit by American guy

--- src/test/java/tests/PS.java
+++ src/test/java/tests/PS.java
@@ -2,9 +2,13 @@ package tests;
 
 public class PS {
 	
+	
 	public void doThis()
 	{
 		System.out.println("do this method..");
+		System.out.println("Jira 1");
+		System.out.println("Jira 2");
 	}
+	
 
 }
